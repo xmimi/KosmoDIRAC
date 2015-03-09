c6c979c6e33c2e1c1edca0508048127dd04e4e43
Zuxuan Qian <zqian@cppm.in2p3.fr>
2015-03-09 23:17:34 +0100

    valid shell

--- post-install.sh
+++ post-install.sh
@@ -1,3 +1,8 @@
-rsync -avzn WebApp ../WebAppDIRAC
-
+#!/bin/bash
+rsync -a WebApp ../WebAppDIRAC
+# vzn = verbose, compress, dry
 # change web.cfg
+cd ../WebAppDIRAC/WebApp
+#mv web.cfg web.cfg.bck
+#cat web.cfg.bck | sed -e "s|DIRAC = link.*|Projects\n    {\n        KosmoUI = DIRAC\.Cosmomc\n    }\n\n    DIRAC = link\|http://diracgrid.org|" > web.cfg
+sed -i -e "s#DIRAC = link.*#Projects\n    {\n        KosmoUI = DIRAC\.Cosmomc\n    }\n\n    DIRAC = link\|http://diracgrid.org#" web.cfg
